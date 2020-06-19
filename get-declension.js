3ee85b7b2ed88cae09565cdd07caf8c80e1174fe
Vladimir Lysov <dubaua@gmail.com>
2020-06-19 11:57:10 +0500

    add type error handling and error tests

--- get-declension.js
+++ get-declension.js
@@ -1,4 +1,17 @@
 function getDeclension({ count, single, few, many }) {
+  if (typeof count !== 'number' || !isFinite(count)) {
+    throw new TypeError(`Expected count is a finite number, got ${typeof count} ${count}`);
+  }
+  if (typeof single !== 'string') {
+    throw new TypeError(`Expected single is a string, got ${typeof single} ${single}`);
+  }
+  if (typeof few !== 'string') {
+    throw new TypeError(`Expected few is a string, got ${typeof few} ${few}`);
+  }
+  if (typeof many !== 'string') {
+    throw new TypeError(`Expected many is a string, got ${typeof many} ${many}`);
+  }
+
   const isFactional = Math.round(count) !== count;
   let declension = many;
   if (isFactional) {
